cb5ca62b9c3ad9bafdbc45d0e0a65358114017a8
Bryon Wicklund <bryon.wicklund@gmail.com>
2019-05-13 11:46:28 -0500

    Updates

--- app/assets/javascripts/administrate-field-lazy_has_many/components/lazy_has_many.js
+++ app/assets/javascripts/administrate-field-lazy_has_many/components/lazy_has_many.js
@@ -1,112 +1,131 @@
 function bindLazyHasManys() {
-  const lazySelects = document.querySelectorAll('[data-component="lazy-has-many"]')
+  const lazySelects = document.querySelectorAll(
+    '[data-component="lazy-has-many"]'
+  );
 
   lazySelects.forEach(lazySelect => {
-    const input = lazySelect.querySelector('input[type="search"]')
-    const button = lazySelect.querySelector('button[type="button"]')
-    const popout = lazySelect.querySelector('[data-target="popout"]')
-    const output = lazySelect.querySelector('[data-target="output"]')
-    const select = output.querySelector('.results-select')
+    const input = lazySelect.querySelector('input[type="search"]');
+    const selected = lazySelect.querySelector(".selected");
+    const popout = lazySelect.querySelector('[data-target="popout"]');
+    const output = lazySelect.querySelector('[data-target="output"]');
+    const select = output.querySelector(".results-select");
+    const options = JSON.parse(lazySelect.getAttribute("data-lazy-has-many"));
 
-    const options = JSON.parse(lazySelect.getAttribute('data-lazy-has-many'))
+    let controller = undefined;
+    let lastResult = undefined;
+    let lastDebounce = undefined;
 
-    let controller = undefined
-    let lastResult = undefined
-    let lastDebounce = undefined
-
-    function onQuery (event) {
+    function onQuery(event) {
+      const value = event.currentTarget.value;
+      console.log(value);
 
       if (controller) {
-        // abort the previous request
-        controller.abort()
+        controller.abort();
       }
 
       if (lastDebounce) {
-        clearTimeout(lastDebounce)
+        clearTimeout(lastDebounce);
       }
 
-      controller = new AbortController()
-      const { signal } = controller
+      controller = new AbortController();
+      const { signal } = controller;
 
       lastDebounce = setTimeout(() => {
-        lastDebounce = undefined
+        lastDebounce = undefined;
 
-        fetch(options.url.replace('{q}', value).replace('%7Bq%7D', value), {
+        fetch(options.url.replace("{q}", value).replace("%7Bq%7D", value), {
           signal,
-          headers: { Accept: 'application/json' }
+          headers: { Accept: "application/json" }
         })
           .then(r => r.json())
-          .then(r => r.map(e => ({ value: e[options.value], label: e[options.label] })))
+          .then(r =>
+            r.map(e => ({ value: e[options.value], label: e[options.label] }))
+          )
           .then(rs => {
-            const currentResult = JSON.stringify((rs))
+            const currentResult = JSON.stringify(rs);
 
             if (lastResult && lastResult === currentResult) {
-              return
+              return;
             }
 
-            lastResult = currentResult
+            lastResult = currentResult;
 
             while (select.lastChild) {
               select.removeChild(select.lastChild);
             }
 
-            const currentValue = target.value
+            // const currentValue = event.currentTarget.value
 
             rs.forEach(r => {
-              const option = document.createElement('option')
-              option.setAttribute('value', r.value)
-              option.innerText = r.label
-              option.selected = currentValue === r.value
-              select.appendChild(option)
-            })
+              const option = document.createElement("option");
+              option.setAttribute("value", r.value);
+              option.innerText = r.label;
+              select.appendChild(option);
+            });
 
-            select.setAttribute('size', "" + Math.max(2, Math.min(Number(select.getAttribute('data-max-size')), rs.length)))
+            select.setAttribute(
+              "size",
+              "" +
+                Math.max(
+                  2,
+                  Math.min(
+                    Number(select.getAttribute("data-max-size")),
+                    rs.length
+                  )
+                )
+            );
           })
           .catch(error => {
-            if (error.name === 'AbortError') {
-              return /* ignore, this is an aborted promise */
+            if (error.name === "AbortError") {
+              return; /* ignore, this is an aborted promise */
             }
-            console.error(error)
-          })
-      }, 250)
+            console.error(error);
+          });
+      }, 250);
     }
 
     function showPopout() {
-      popout.classList.add('active')
+      popout.classList.add("active");
+      input.focus()
     }
 
     function hidePopout() {
-      popout.classList.remove('active')
+      popout.classList.remove("active");
     }
 
-    input.addEventListener('input', onQuery)
-    button.addEventListener('click', showPopout)
+    input.addEventListener("input", onQuery);
+    selected.addEventListener("click", showPopout);
 
-    document.addEventListener('click', (e) => {
-      const lazy = e.target && e.target.closest('[data-component="lazy-has-many"]')
+    document.addEventListener("click", e => {
+      const lazy =
+        e.target && e.target.closest('[data-component="lazy-has-many"]');
       if (lazy !== lazySelect) {
-        hidePopout()
+        hidePopout();
       }
-    })
+    });
 
-    select.addEventListener('change', (e) => {
-      if (!e.currentTarget.value) {
-        return
+    select.addEventListener("click", e => {
+      option = new Option(
+        e.currentTarget.selectedOptions[0].textContent,
+        e.currentTarget.value,
+        false,
+        true
+      );
+      selected.append(option);
+      for (var i=0; i<selected.options.length; i++) {
+        selected.options[i].selected = true;
       }
+      hidePopout();
+    });
 
-      target.value = e.currentTarget.value
-      button.textContent = e.currentTarget.selectedOptions[0].textContent
-      hidePopout()
-    })
-
-    button.removeAttribute('disabled')
-  })
+    selected.removeAttribute("disabled");
+  });
 }
 
 if (window.Turbolinks && window.Turbolinks.supported) {
-  document.addEventListener("turbolinks:load", function () {
-    bindLazyHasManys()
-  })
+  document.addEventListener("turbolinks:load", function() {
+    bindLazyHasManys();
+  });
 } else {
-  document.addEventListener('DOMContentLoaded', bindLazyHasManys)
+  document.addEventListener("DOMContentLoaded", bindLazyHasManys);
 }
